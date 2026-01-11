9c36dce1925dbdded116c158ebbed79927afd99f
gabiseabra <hey@gabiseabra.dev>
2026-01-11 20:27:59 +0100

    github source badge

--- web/src/components/ui/Root.tsx
+++ web/src/components/ui/Root.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import { Link } from "react-router";
+import { FaGithub } from "react-icons/fa";
 import css from "./Root.module.scss";
 
 export function Root({ children }: { children: ReactNode }) {
@@ -13,6 +14,17 @@ export function Root({ children }: { children: ReactNode }) {
           <span className={css.Dot}>.</span>
           <span className={css.Domain}>dev</span>
         </Link>
+
+        <a
+          target="_blank"
+          href="https://github.com/gabiseabra/notion-site"
+          title="See the source code on Github"
+          className={css.Github}
+        >
+          <div>
+            <FaGithub />
+          </div>
+        </a>
       </header>
 
       <main>{children}</main>
