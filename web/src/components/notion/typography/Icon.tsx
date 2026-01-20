c13ddea0da845e40a4f037b0692cf34626759742
Gabriela Seabra <hey@gabiseabra.dev>
2026-01-20 20:43:59 +0100

    IconButton

--- web/src/components/notion/typography/Icon.tsx
+++ web/src/components/notion/typography/Icon.tsx
@@ -1,39 +1,27 @@
 import * as zN from "@notion-site/common/dto/notion/schema.js";
 import { match } from "ts-pattern";
-import { ComponentProps } from "react";
+import { IconButton, IconButtonProps } from "../../typography/IconButton.js";
 
 type IconProps = {
+  size: IconButtonProps["size"];
   icon: zN.icon;
-  size: "xs" | "s" | "m" | "l";
-} & ComponentProps<"div">;
+};
 
 /**
  * Renders a Notion icon.
  * @direction inline
  */
-export function Icon({ icon, size, style = {}, ...props }: IconProps) {
-  const width = { xs: "14px", s: "18px", m: "24px", l: "32px" }[size];
-
+export function Icon({ icon, size }: IconProps) {
   return (
-    <span
-      style={{
-        width,
-        aspectRatio: 1,
-        display: "inline-block",
-        ...style,
-      }}
-      {...props}
-    >
+    <IconButton as="span" size={size}>
       {match(icon)
-        .with({ type: "emoji" }, (icon) => (
-          <span style={{ fontSize: width }}>{icon.emoji}</span>
-        ))
+        .with({ type: "emoji" }, (icon) => icon.emoji)
         .with({ type: "custom_emoji" }, (icon) => (
           <img src={icon.custom_emoji.url} />
         ))
         .with({ type: "external" }, (icon) => <img src={icon.external.url} />)
         .with({ type: "file" }, (icon) => <img src={icon.file.url} />)
         .exhaustive()}
-    </span>
+    </IconButton>
   );
 }
---
+++ web/src/components/typography/IconButton.tsx
@@ -0,0 +1,54 @@
+import { CSSProperties, ReactNode } from "react";
+import { isTruthy } from "@notion-site/common/utils/guards.js";
+import * as zn from "@notion-site/common/dto/notion/schema.js";
+import styles from "./IconButton.module.scss";
+
+export type IconButtonProps = {
+  as: "span" | "a" | "button";
+
+  size: "xs" | "s" | "m" | "l";
+  color?: zn.color | "primary" | "secondary";
+
+  badge?: string;
+
+  children: ReactNode;
+  className?: string;
+  style?: CSSProperties;
+  title?: string;
+
+  onClick?: () => void;
+};
+
+/**
+ * @direction inline
+ */
+export function IconButton({
+  as: Component,
+  size,
+  color,
+  badge,
+  children,
+  className,
+  onClick,
+  ...props
+}: IconButtonProps) {
+  return (
+    <Component
+      className={[
+        className,
+        styles.icon,
+        styles[`size-${size}`],
+        styles[`color-${color}`],
+        onClick && styles.clickable,
+      ]
+        .filter(isTruthy)
+        .join(" ")}
+      onClick={() => onClick?.()}
+      {...props}
+    >
+      {children}
+
+      {!!badge && <span className={styles.badge}>{badge}</span>}
+    </Component>
+  );
+}
