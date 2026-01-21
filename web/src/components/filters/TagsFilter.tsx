9bfcb0d23d9696c443d70298b11d0252210e6bcb
gabiseabra <hey@gabiseabra.dev>
2026-01-21 18:49:55 +0100

    use Tooltip for TagsFilter description

--- web/src/components/filters/TagsFilter.tsx
+++ web/src/components/filters/TagsFilter.tsx
@@ -1,6 +1,8 @@
 import * as zn from "@notion-site/common/dto/notion/schema.js";
 import { Row } from "../layout/FlexBox.js";
 import { Badge, BadgeProps } from "../typography/Badge.js";
+import { Tooltip } from "../layout/Tooltip.js";
+import { Text } from "../typography/Text.js";
 
 export type TagsFilterProps<T extends string> = {
   options: {
@@ -21,30 +23,44 @@ export function TagsFilter<T extends string>({
   return (
     <Row wrap>
       {options.map((option) => (
-        <a
-          key={option.name}
-          title={option.description ?? undefined}
-          onClick={() =>
-            onChange(
-              value.includes(option.name)
-                ? value.filter((v) => v !== option.name)
-                : [...value, option.name],
-            )
+        <Tooltip
+          disabled={!option.description}
+          content={
+            <Text
+              as="div"
+              size="caption"
+              color="muted"
+              p={1}
+              style={{ maxWidth: 250 }}
+            >
+              {option.description}
+            </Text>
           }
         >
-          <Badge
-            size="s"
-            status={option.status}
-            color={option.color}
-            style={{
-              outline: value.includes(option.name)
-                ? "1px solid currentColor"
-                : undefined,
-            }}
+          <a
+            key={option.name}
+            onClick={() =>
+              onChange(
+                value.includes(option.name)
+                  ? value.filter((v) => v !== option.name)
+                  : [...value, option.name],
+              )
+            }
           >
-            {option.name}
-          </Badge>
-        </a>
+            <Badge
+              size="s"
+              status={option.status}
+              color={option.color}
+              style={{
+                outline: value.includes(option.name)
+                  ? "1px solid currentColor"
+                  : undefined,
+              }}
+            >
+              {option.name}
+            </Badge>
+          </a>
+        </Tooltip>
       ))}
     </Row>
   );
