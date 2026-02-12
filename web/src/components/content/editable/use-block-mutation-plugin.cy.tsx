5dd82720007febaab47db261e990100c4da5dd64
gabiseabra <hey@gabiseabra.dev>
2026-02-12 21:47:00 +0100

    block and inline mutation tests

---
+++ web/src/components/content/editable/use-block-mutation-plugin.cy.tsx
@@ -0,0 +1,150 @@
+import { p, span } from "@notion-site/common/utils/notion/wip.js";
+import { SelectionRange } from "../../../utils/selection-range.js";
+import { ContentEditor } from "../ContentEditor.js";
+
+describe("useBlockMutationPlugin", () => {
+  it("merges with previous block on backspace at start of block", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("First")), p("b", span("Second"))]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p").eq(1).click().type("{moveToStart}{backspace}");
+
+    cy.get("p").should("have.length", 1);
+    cy.get("p").should("have.text", "FirstSecond");
+  });
+
+  it("does nothing on backspace at start of first block", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("First")), p("b", span("Second"))]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p").eq(0).click().type("{moveToStart}{backspace}");
+
+    cy.get("p").should("have.length", 2);
+    cy.get("p").eq(0).should("have.text", "First");
+    cy.get("p").eq(1).should("have.text", "Second");
+  });
+
+  it("deletes empty block on backspace", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("First")), p("b")]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p").eq(1).click().type("{backspace}").should("not.exist");
+
+    cy.get("p").should("have.length", 1);
+    cy.get("p").should("have.text", "First");
+  });
+
+  it("splits block at caret position on Enter in middle of text", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("HelloWorld"))]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p").click().type("{moveToStart}");
+    for (let i = 0; i < 5; i++) {
+      cy.get("p").eq(0).type("{rightArrow}");
+    }
+    cy.get("p").eq(0).type("{enter}");
+
+    cy.get("p").should("have.length", 2);
+    cy.get("p").eq(0).should("have.text", "Hello");
+    cy.get("p").eq(1).should("have.text", "World");
+  });
+
+  it("creates empty block above on Enter at start of block", () => {
+    cy.mount(
+      <ContentEditor value={[p("a", span("Hello"))]} onChange={() => {}} />,
+    );
+
+    cy.get("p").click().type("{moveToStart}{enter}");
+
+    cy.get("p").should("have.length", 2);
+    cy.get("p").eq(0).should("have.text", "");
+    cy.get("p").eq(1).should("have.text", "Hello");
+  });
+
+  it("creates empty block below on Enter at end of block", () => {
+    cy.mount(
+      <ContentEditor value={[p("a", span("Hello"))]} onChange={() => {}} />,
+    );
+
+    cy.get("p").click().type("{end}{enter}");
+
+    cy.get("p").should("have.length", 2);
+    cy.get("p").eq(0).should("have.text", "Hello");
+    cy.get("p").eq(1).should("have.text", "");
+  });
+
+  it("does not split on Shift+Enter", () => {
+    cy.mount(
+      <ContentEditor value={[p("a", span("Hello"))]} onChange={() => {}} />,
+    );
+
+    cy.get("p").click().type("{end}{shift}{enter}");
+
+    cy.get("p").should("have.length", 1);
+  });
+
+  it("focuses the new block after Enter split", () => {
+    cy.mount(
+      <ContentEditor value={[p("a", span("Hello"))]} onChange={() => {}} />,
+    );
+
+    cy.get("p").click().type("{end}{enter}");
+
+    cy.get("p").eq(1).should("have.focus");
+  });
+
+  it("focuses the previous block after merge", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("First")), p("b", span("Second"))]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p")
+      .eq(1)
+      .click()
+      .type("{moveToStart}{backspace}")
+      .should("not.exist");
+
+    cy.get("p").eq(0).should("have.focus");
+  });
+
+  it("places caret at merge boundary after merge", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("First")), p("b", span("Second"))]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p")
+      .eq(1)
+      .click()
+      .type("{moveToStart}{backspace}")
+      .should("not.exist");
+
+    // Caret should be at position 5 (after "First")
+    cy.get("p")
+      .eq(0)
+      .then(([p]) => {
+        expect(SelectionRange.read(p)).to.deep.equal({ start: 5, end: 5 });
+      });
+  });
+});
---
+++ web/src/components/content/editable/use-inline-mutation-plugin.cy.tsx
@@ -0,0 +1,152 @@
+import { p, span } from "@notion-site/common/utils/notion/wip.js";
+import { ContentEditor } from "../ContentEditor.js";
+
+const options = {
+  autoCommit: 200,
+};
+
+describe("useInlineMutationPlugin", () => {
+  it.skip("inserts newline on Shift+Enter in empty block", () => {
+    cy.mount(
+      <ContentEditor value={[p("a")]} onChange={() => {}} options={options} />,
+    );
+
+    cy.get("p").click().type("{shift}{enter}").should("have.text", "\n");
+
+    // check that before and after commit content is the same
+    cy.wait(options.autoCommit);
+
+    cy.get("p span").should("have.text", "\n");
+  });
+
+  it.skip("inserts newline on Shift+Enter at end of block with text", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("Hello"))]}
+        onChange={() => {}}
+        options={options}
+      />,
+    );
+
+    cy.get("p")
+      .click()
+      .type("{end}{shift}{enter}more")
+      .should("have.text", "Hello\nmore");
+
+    cy.wait(options.autoCommit);
+
+    cy.get("p span").should("have.text", "Hello\nmore");
+  });
+
+  it("prevents Shift+Enter newline when multiline is disabled", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("Hello"))]}
+        onChange={() => {}}
+        options={{ ...options, multiline: false }}
+      />,
+    );
+
+    // Should still be single line, no newline inserted
+    cy.get("p")
+      .click()
+      .type("{end}{shift}{enter}")
+      .should("have.text", "Hello");
+
+    cy.wait(options.autoCommit);
+
+    cy.get("p").should("have.text", "Hello");
+  });
+
+  it("handles type characters then backspace some", () => {
+    cy.mount(
+      <ContentEditor value={[p("a")]} onChange={() => {}} options={options} />,
+    );
+
+    cy.get("p").click().type("Hello World").should("have.text", "Hello World");
+
+    cy.wait(options.autoCommit);
+
+    cy.get("p")
+      .type("{backspace}{backspace}{backspace}{backspace}{backspace}")
+      .should("have.text", "Hello ");
+
+    cy.wait(options.autoCommit);
+
+    cy.get("p").should("have.text", "Hello ");
+  });
+
+  it("selects text then types to replace it", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("Hello World"))]}
+        onChange={() => {}}
+        options={options}
+      />,
+    );
+
+    cy.get("p")
+      .click()
+      .type("{selectAll}Replaced")
+      .should("have.text", "Replaced");
+
+    cy.wait(options.autoCommit);
+
+    cy.get("p").should("have.text", "Replaced");
+  });
+
+  it("selects text then deletes it", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("Hello World"))]}
+        onChange={() => {}}
+        options={options}
+      />,
+    );
+
+    cy.get("p").click().type("{selectAll}{del}").should("have.text", "");
+
+    cy.wait(options.autoCommit);
+
+    cy.get("p").should("have.text", "");
+  });
+
+  it("inserts text via execCommand (simulates paste)", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("Before "))]}
+        onChange={() => {}}
+        options={options}
+      />,
+    );
+
+    cy.get("p").click().type("{end}");
+
+    cy.document().then((doc) => {
+      doc.execCommand("insertText", false, "pasted text");
+    });
+
+    cy.get("p").should("have.text", "Before pasted text");
+
+    cy.wait(options.autoCommit);
+
+    cy.get("p").should("have.text", "Before pasted text");
+  });
+
+  it("flushes pending changes before block split", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("Initial"))]}
+        onChange={() => {}}
+        options={options}
+      />,
+    );
+
+    // Type rapidly then immediately press Enter (which triggers block-mutation plugin)
+    // The typed text should not disappear (proves flush works before split)
+    cy.get("p").click().type("{selectAll}First line{enter}");
+
+    cy.get("p").eq(0).should("have.text", "First line");
+    cy.get("p").eq(1).should("exist");
+  });
+});
