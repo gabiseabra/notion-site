c193a083f41dfb6265d26e4817d17a4f1434ecd2
Gabriela Seabra <hey@gabiseabra.dev>
2026-02-12 10:07:57 +0100

    use-block-navigation-plugin multiline block     test

--- web/src/components/content/editable/use-block-navigation-plugin.cy.tsx
+++ web/src/components/content/editable/use-block-navigation-plugin.cy.tsx
@@ -65,4 +65,44 @@ describe("useBlockNavigationPlugin", () => {
 
     cy.get("@first").should("have.focus");
   });
+
+  it("moves caret to next block on ArrowDown when caret is in the last line", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("Multi\nLine\nString")), p("b", span("Second"))]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p").eq(0).as("first");
+    cy.get("p").eq(1).as("second");
+
+    cy.get("@first").click().type("{downArrow}{downArrow}");
+
+    cy.get("@first").should("have.focus");
+
+    cy.get("@first").click().type("{downArrow}");
+
+    cy.get("@second").should("have.focus");
+  });
+
+  it("moves caret to previous block on ArrowUp when caret is in the first line", () => {
+    cy.mount(
+      <ContentEditor
+        value={[p("a", span("First")), p("b", span("Multi\nLine\nString"))]}
+        onChange={() => {}}
+      />,
+    );
+
+    cy.get("p").eq(0).as("first");
+    cy.get("p").eq(1).as("second");
+
+    cy.get("@second").click().type("{upArrow}{upArrow}");
+
+    cy.get("@second").should("have.focus");
+
+    cy.get("@second").click().type("{upArrow}");
+
+    cy.get("@first").should("have.focus");
+  });
 });
