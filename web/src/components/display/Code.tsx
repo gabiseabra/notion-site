f88fbcbeb32d8e7542f16f9de65c3423b39ae127
Gabriela Seabra <hey@gabiseabra.dev>
2026-02-20 13:09:06 +0100

    adjuustments

--- web/src/components/display/Code.tsx
+++ web/src/components/display/Code.tsx
@@ -3,6 +3,7 @@ import Prism from "prismjs";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-markup";
+import "prismjs/components/prism-scss";
 import "prismjs/components/prism-tsx";
 import "prismjs/components/prism-typescript";
 import { ReactNode, useMemo } from "react";
