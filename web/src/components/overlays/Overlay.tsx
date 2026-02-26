10f2f414f2ddf192a84bb99eb5c14bd34e2c70d9
Gabriela Seabra <hey@gabiseabra.dev>
2026-02-26 11:36:48 +0100

    remove unused

--- web/src/components/overlays/Overlay.tsx
+++ web/src/components/overlays/Overlay.tsx
@@ -1,4 +1,4 @@
-import { ComponentType, ReactNode, useState } from "react";
+import { ComponentType, ReactNode } from "react";
 
 export type OverlayProps = {
   open: boolean;
@@ -11,25 +11,3 @@ export type AnchoredOverlayProps = {
   content: ReactNode;
 } & OverlayProps;
 export type AnchoredOverlay = ComponentType<AnchoredOverlayProps>;
-
-export function ControlledOverlay<Props extends object>({
-  as: Overlay,
-  disabled,
-  children,
-  ...props
-}: {
-  as: ComponentType<Props & OverlayProps>;
-  disabled?: boolean;
-  children: (overlay: { isOpen: boolean; open: () => void }) => ReactNode;
-} & Props) {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const overlayProps = {
-    ...props,
-    open: isOpen,
-    onClose: () => setIsOpen(false),
-    children: children({ isOpen, open: () => setIsOpen(!disabled && true) }),
-  } as Props & OverlayProps;
-
-  return <Overlay {...overlayProps} />;
-}
