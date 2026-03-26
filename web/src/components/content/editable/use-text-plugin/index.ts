3acd392f76792c184babffa422df4d707bd42bcb
Gabriela Seabra <hey@gabiseabra.dev>
2026-03-26 07:45:27 -0300

    use-text-plugin logging

--- web/src/components/content/editable/use-text-plugin/index.ts
+++ web/src/components/content/editable/use-text-plugin/index.ts
@@ -1,9 +1,12 @@
+import * as env from "../../../../env.js";
 import { SpliceRange } from "../../../../utils/splice-range.js";
+import { EditorEvent } from "../../editor/editor-event";
 import { ContentEditor } from "../../editor/types.js";
 import { composePlugins } from "../compose-plugins.js";
 import { useAutoCommitPlugin } from "../use-auto-commit-plugin.js";
 import { useHistoryPlugin } from "../use-history-plugin.js";
 import { useInlineMutationPlugin } from "../use-inline-mutation-plugin.js";
+import { useLoggerPlugin } from "../use-logger-plugin";
 
 export type TextBlock = {
   id: string;
@@ -33,15 +36,30 @@ export const TextBlock = {
 export type TextPluginOptions = {
   multiLine?: boolean;
   autoCommit?: number | boolean;
+  logging?: boolean | "verbose";
 };
 
 /**
  * ContentEditor plugin stack for plain text blocks.
  */
 export const useTextPlugin =
-  ({ multiLine = true, autoCommit = 600 }: TextPluginOptions = {}) =>
+  ({
+    multiLine = true,
+    autoCommit = 600,
+    logging = env.DEV,
+  }: TextPluginOptions = {}) =>
   (editor: ContentEditor<TextBlock>) =>
     composePlugins<TextBlock>(
+      useLoggerPlugin((event) => {
+        if (!logging) return;
+        else if (logging == "verbose")
+          console.info(event.eventType, event.detail, event.editor);
+        else if (
+          !EditorEvent.narrow("flush", event) &&
+          !EditorEvent.narrow("postcommit", event)
+        )
+          console.info(event.eventType, event.detail, event.editor);
+      }),
       useAutoCommitPlugin({
         disabled: autoCommit === false,
         debounceMs: typeof autoCommit === "number" ? autoCommit : undefined,
@@ -50,6 +68,5 @@ export const useTextPlugin =
       useInlineMutationPlugin({
         multiLine,
         splice: TextBlock.splice,
-        change: ({ id }, value) => ({ id, value }),
       }),
     )(editor);
