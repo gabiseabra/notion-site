7b0822090aa8182503cac42fa07bf0fd896c5efc
Gabriela Seabra <hey@gabiseabra.dev>
2026-04-07 00:49:56 -0300

    logs wip

--- web/src/components/content/editable/use-notion-plugin/use-notion-prefix-plugin.cy.tsx
+++ web/src/components/content/editable/use-notion-plugin/use-notion-prefix-plugin.cy.tsx
@@ -69,15 +69,19 @@ describe("useNotionPrefixPlugin", () => {
     cy.get("label").should("have.text", "test");
   });
 
-  it("converts paragraph to numbered list on typing the next number below a numbered list", () => {
-    cy.mount(<Editor value={[p("1"), p("2")]} onChange={() => {}} />);
+  it.only("converts paragraph to numbered list on typing the next number below a numbered list", () => {
+    cy.mount(<TestEditor value={[p("1"), p("2")]} onChange={() => {}} />);
 
-    cy.get("p")
-      .eq(0)
-      .click()
-      .realType("1. a{enter}b")
-      .realPress("ArrowDown")
-      .realType("3. c");
+    cy.get("p").eq(0).click().type("1. a{enter}b{downArrow}3. c", {
+      log: true,
+    });
+
+    // cy.get("p")
+    //   .eq(0)
+    //   .click()
+    //   .realType("1. a{enter}b")
+    //   .realPress("ArrowDown")
+    //   .realType("3. c");
 
     cy.get("li").should("have.length", 3);
     cy.get("li").eq(0).should("have.text", "a");
