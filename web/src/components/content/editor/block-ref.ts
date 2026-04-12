bf4b2cc237eaafa641f0653ed4eeb4abdf1abc8b
Gabriela Seabra <hey@gabiseabra.dev>
2026-04-11 21:29:24 -0300

    BlockRef.all

--- web/src/components/content/editor/block-ref.ts
+++ web/src/components/content/editor/block-ref.ts
@@ -1,5 +1,5 @@
 import { isNonNullable } from "@notion-site/common/utils/guards.js";
-import { ID } from "./types";
+import { AnyBlock, ContentEditor, ID } from "./types";
 
 export type BlockRef = {
   element: HTMLElement | null;
@@ -7,6 +7,17 @@ export type BlockRef = {
 };
 
 export const BlockRef = {
+  all(editor: ContentEditor<AnyBlock>) {
+    return editor.blocks
+      .flatMap((block) => BlockRef.entries(editor.ref(block.id)))
+      .sort(([, a], [, b]) => {
+        const position = a.compareDocumentPosition(b);
+        if (position & Node.DOCUMENT_POSITION_FOLLOWING) return -1;
+        if (position & Node.DOCUMENT_POSITION_PRECEDING) return 1;
+        return 0;
+      });
+  },
+
   entries(ref: BlockRef) {
     return [
       ref.element && ([undefined as ID | undefined, ref.element] as const),
